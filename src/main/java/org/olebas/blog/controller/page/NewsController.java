03176b1748c46d63432a8bf7ff8cc3ed35d1ea7e
Oleg N <olebas13@gmail.com>
2020-11-05 15:59:35 +0200

    added NewsController

---
+++ src/main/java/org/olebas/blog/controller/page/NewsController.java
@@ -0,0 +1,19 @@
+package org.olebas.blog.controller.page;
+
+import org.olebas.blog.controller.AbstractController;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet({"/news", "/news/*"})
+public class NewsController extends AbstractController {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        forwardToPage("news.jsp", req, resp);
+    }
+}
